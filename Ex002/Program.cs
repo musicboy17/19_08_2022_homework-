dca7c27ea89c6f0bd7413e7c1e68b57973fc49e0
musicboy17 <pratham@yandex.ru>
2022-08-19 22:15:46 +0300

    Решение 27-ой задачи_002

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -9,8 +9,42 @@
 
 Console.WriteLine("Введите число А: ");
 int Number_A = Convert.ToInt32(Console.ReadLine());
+string sen = Convert.ToString(Number_A);
+int i = 0;
+int j = 0;
+int rez = 1;
+int l01 = 10;
+int Zumm = 0;
 
-Console.WriteLine($"число А: {Number_A}");
 
-result01 =  
+for (i = 1; i <= sen.Length; i++)
 
+{
+    int result =  Number_A % 10;
+    Console.WriteLine($"число {i}: {result}");
+    
+    for (j=1; j <= sen.Length; j++)
+    rez = Number_A / l01 * 10 ;
+    Number_A = Number_A / 10;
+
+    int result02 =  rez % 10;
+    l01=l01*10;
+    Zumm = Zumm + result;
+}
+
+// Console.WriteLine($"число А: {Number_A}");
+
+// int result01 =  Number_A % 10;
+
+// Console.WriteLine($"число 1: {result01}");
+
+// int result02 =  (Number_A /10) % 10;
+
+// Console.WriteLine($"число 2: {result02}");
+
+// int result03 =  (Number_A /100) % 10;
+
+// Console.WriteLine($"число 3: {result03}");
+
+Console.WriteLine($"порядок числа А: {sen.Length}");
+Console.WriteLine($"Сумма цифр в числе А: {Zumm}");
