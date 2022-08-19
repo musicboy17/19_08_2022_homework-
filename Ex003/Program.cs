76de86822b9de9253585fc89aff93916c16f88b4
musicboy17 <pratham@yandex.ru>
2022-08-19 22:45:09 +0300

    Решение 29-ой задачи_002

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -5,6 +5,16 @@
 
 // 6, 1, 33 -> [6, 1, 33]
 
+int [] array_new = {10, 8, 1, 3, 5, 5, 7,7};
+int i = 0;
+void printarray(int[] array)
+{
+    int Count = array.Length;
+    for (int i = 0; i < Count; i++);
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 
-
-
+}
+printarray(array_new);
